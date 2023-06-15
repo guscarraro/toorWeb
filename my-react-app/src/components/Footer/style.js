93b942de5037f692a33ce2a1c08c45f892316479
guscarraro <gus.carraro@hotmail.com>
2023-06-15 15:19:58 -0300

    fix:entrega final

--- my-react-app/src/components/Footer/style.js
+++ my-react-app/src/components/Footer/style.js
@@ -12,7 +12,7 @@ export const FooterFullStyle = styled.footer`
   align-items: center;
   flex-direction: column;
   width: 100%;
-  height: 80vh;
+  height: 100vh;
   padding: 0;
   margin: 0;
   color: white;
